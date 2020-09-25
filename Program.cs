0a34d9137baf98cfd22ec6ab9dd53bc145f462d5
Joe Brooks <jrb092382@gmail.com>
2020-09-25 15:21:58 -0400

    Started 9/25 homework

--- Program.cs
+++ Program.cs
@@ -4,6 +4,26 @@ namespace Choose_Your_Class
 {
     class Program
     {
+        // ***GUITAR BUILDER***
+
+        // what type of guitar do you want to build?
+        // 1. left or right handed
+        // 2. acoustic or electric
+        // 3. Guitar or Bass
+        // 4. number of strings 
+            // (Guitar - 6,7,8) (Bass - 4,5,6)
+        // 5. number of frets (21, 22, 24)
+        // 6. color
+
+        // tone and specs!
+        // 1. tone wood
+        // 2. pickups
+        // 3. fixed bridge or tremolo
+        
+
+
+
+
         static void Main(string[] args)
         {
             
